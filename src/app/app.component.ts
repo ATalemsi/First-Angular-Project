d3db0c16664eb78156c6bbccd27d3aa7e6752396
ATalemsi <mohamadtalemsi@gmail.com>
2024-12-25 15:28:36 +0100

    management the categories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from "./navbar/navbar.component";
+import {AsideComponent} from "./aside/aside.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, AsideComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-app';
+  isAsideOpen = false;
+
+  toggleAside() {
+    this.isAsideOpen = !this.isAsideOpen;
+  }
+
 }
