b19f325637310b0d7e9a2d6c65e954d57f4df27a
ATalemsi <mohamadtalemsi@gmail.com>
2024-12-25 11:32:42 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {CategoryComponent} from "./categories/category.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'categories', component: CategoryComponent }, // Route for Categories
+  { path: '', redirectTo: '/categories', pathMatch: 'full' }, // Redirect to Categories as default
+  { path: '**', redirectTo: '/categories' }, // Wildcard route for 404
+];
